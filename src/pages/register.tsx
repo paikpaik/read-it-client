a135e2e678e61ae243e7cb5fcd3eb1b6357e9e86
paikpaik <woduf9311@gmail.com>
2023-12-24 23:29:23 +0900

    Add : InputGroup components로 email, username, password form 추가

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,13 +1,36 @@
+import InputGroup from "@/components/InputGroup";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
-const register = () => {
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<any>({});
   return (
     <div className="bg-white">
       <div className="flex flex-col items-center justify-center h-screen p-6">
         <div className="w-10/12 mx-auto md:w-96">
           <h1 className="mb-2 text-lg font-medium">회원가입</h1>
           <form>
+            <InputGroup
+              placeholder="Email"
+              value={email}
+              setValue={setEmail}
+              error={errors.email}
+            />
+            <InputGroup
+              placeholder="Username"
+              value={username}
+              setValue={setUsername}
+              error={errors.username}
+            />
+            <InputGroup
+              placeholder="Password"
+              value={password}
+              setValue={setPassword}
+              error={errors.password}
+            />
             <button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border-gray-400 rounded">
               회원 가입
             </button>
@@ -24,4 +47,4 @@ const register = () => {
   );
 };
 
-export default register;
+export default Register;
