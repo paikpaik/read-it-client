203676c1104ef6cb6b71e86d1932661c4e5d6fe0
paikpaik <woduf9311@gmail.com>
2023-12-28 01:12:19 +0900

    Add : context & useReducer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
+import { AuthProvider } from "@/context/auth";
 import "@/styles/globals.css";
 import Axios from "axios";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />;
+    </AuthProvider>
+  );
 }
